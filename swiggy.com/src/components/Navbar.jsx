d7511f503e38ca231bb3cb9338f065ae63fcb4b9
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-10-05 07:22:41 +0530

    update swiggy

--- swiggy.com/src/components/Navbar.jsx
+++ swiggy.com/src/components/Navbar.jsx
@@ -484,29 +484,6 @@ export const Navbar = () => {
   };
 
   return (
-    //    <div id="headerContainer">
-    // <div id="headerContentContainer">
-    //     <Link to=""  id="logo"><img src="https://cdn.worldvectorlogo.com/logos/swiggy-1.svg"
-    //             alt="swiggylogo"/></Link>
-    //     <div id="currentLocation" style={{hover:{color:"orange"}}}>{localStorage.getItem("address")}<KeyboardArrowDownIcon color="#ff3d00"/></div>
-    //     <ul id="headerContainerRight">
-    //         <li id="search">
-    //             <Link  to="/search"><img
-    //                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/2048px-Search_Icon.svg.png"/>Search</Link>
-    //         </li>
-    //         <li id="offer">
-    //             <Link to="" ><img src="https://static.thenounproject.com/png/971055-200.png" alt=""/>
-    //                 Offers</Link>
-    //         </li>
-    //         <li id="help">
-    //             <Link to="" > <img src="https://cdn.pixabay.com/photo/2016/07/23/13/18/pokemon-1536847_960_720.png"
-    //                     alt=""/> Help</Link>
-    //         </li>
-    //         <li id="signIn">
-    //         <Link to=""  id="signInA"><img src="https://www.transparentpng.com/thumb/user/single-user-icon-png-free--rLHSHx.png" alt=""/>Guest</Link>
-    //     </li>
-    //         <li id="cart"><Link to="" > Cart<span id="noOfCartItems"></span></Link></li>
-    //      </ul>
     <div id="headerContainer">
       <div id="headerContentContainer">
         <Drawer
